19ce824b5372c20d2103d50095628571dee547ad
kzzzld <kzzzl@kzzzl.xyz>
2026-09-07 17:56:20 +0300

    feat(gtk): manage gtk automatically

--- home/home.nix
+++ home/home.nix
@@ -7,45 +7,71 @@
 
   home.packages = with pkgs; [
     i3status
-    alacritty
-    vim
-    librewolf
-    chromium
-    picom
-    feh
-    rofi
-    git
-    lazygit
-    fastfetch
-    tmux
-    tmuxinator
-    eza
-    starship
-    zoxide
-    gajim
-    nix-search-cli
-    maim
-    xclip
-    xdotool
-    neovim
-    zip
-    unzip
-    nodejs
-    cargo
-    stylua
-    mpv
-    grim
-    wl-clipboard
-    swaybg
-    gcc
-    tree-sitter
-    catppuccin-cursors.mochaBlue
-    nerd-fonts.jetbrains-mono
-  ];
-  
+      alacritty
+      vim
+      librewolf
+      chromium
+      picom
+      feh
+      rofi
+      git
+      lazygit
+      fastfetch
+      tmux
+      tmuxinator
+      eza
+      starship
+      zoxide
+      gajim
+      nix-search-cli
+      maim
+      xclip
+      xdotool
+      neovim
+      zip
+      unzip
+      nodejs
+      cargo
+      stylua
+      mpv
+      grim
+      wl-clipboard
+      swaybg
+      gcc
+      tree-sitter
+      slurp
+      thunar
+      nerd-fonts.jetbrains-mono
+      ];
+
   xresources.properties = {
     "Xcursor.size" = 24;
     "Xcursor.theme" = "catppuccin-mocha-blue-cursors";
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "catppuccin-mocha-blue-standard";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "blue" ];
+        variant = "mocha";
+      };
+    };
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.catppuccin-papirus-folders.override {
+        flavor = "mocha";
+        accent = "blue";
+      };
+    };
+
+    cursorTheme = {
+      name = "catppuccin-mocha-blue-cursors";
+      package = pkgs.catppuccin-cursors.mochaBlue;
+    };
   };
 
   home.file."Wallpapers".source = ./config/walls;
@@ -59,5 +85,4 @@
     recursive = true;
   };
   home.file.".zshrc".source = ./config/zsh/zshrc;
-  home.file.".config/gtk-3.0".source = ./config/gtk3;
-}
+                      }
