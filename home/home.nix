2e45a25da559a1744f6ae0e1dec32fa1b702cb6a
kzzzld <kzzzl@kzzzl.xyz>
2026-09-07 14:08:04 +0300

    feat(zsh): init

--- home/home.nix
+++ home/home.nix
@@ -15,6 +15,10 @@
     rofi
     git
     lazygit
+    fastfetch
+    tmux
+    tmuxinator
+    eza
     catppuccin-cursors.mochaBlue
     nerd-fonts.jetbrains-mono
   ];
@@ -29,5 +33,6 @@
   home.file.".config/i3status".source = ./config/i3status;
   home.file.".config/alacritty".source = ./config/alacritty;
   home.file.".config/rofi".source = ./config/rofi;
+  home.file.".zshrc".source = ./config/zsh/zshrc;
   home.file.".config/gtk-3.0".source = ./config/gtk3;
 }
