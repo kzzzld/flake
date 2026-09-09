aa692ec6707f8ec796ebc064bf0232e329cf680b
kzzzld <kzzzl@kzzzl.xyz>
2026-09-09 21:36:27 +0300

    feat(gitconfig): init

--- home/home.nix
+++ home/home.nix
@@ -94,6 +94,7 @@
   home.file.".config/rofi".source = ./config/rofi;
   home.file.".config/sway".source = ./config/sway;
   home.file.".config/swaylock".source = ./config/swaylock;
+  home.file.".gitconfig".source = ./config/git/gitconfig;
   # home.file.".xinitrc".source = ./config/xorg/xinitrc;
   home.file.".config/nvim" = {
     source = ./config/nvim;
