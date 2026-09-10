bfa351eb6fb47096de56ab3c07692795d269eab2
kzzzld <kzzzl@kzzzl.xyz>
2026-09-10 14:00:56 +0300

    feat(nixmac-btw): add dnscrypt-proxy and zapret

--- hosts/nixmac-btw/configuration.nix
+++ hosts/nixmac-btw/configuration.nix
@@ -25,6 +25,27 @@
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.backend = "iwd";
 
+  services.resolved.enable = false;
+
+  networking = {
+    nameservers = ["127.0.0.1" "::1"];
+  };
+
+  services.dnscrypt-proxy = {
+    enable = true;
+    settings = {
+      listen_addresses = ["127.0.0.1:53" "[::1]:53"];
+    };
+  };
+
+  services.zapret = {
+    enable = true;
+    params = [
+      "--dpi-desync=fake"
+      "--dpi-desync-ttl=3"
+    ];
+  };
+
   # Set your time zone.
   time.timeZone = "Europe/Istanbul";
 
