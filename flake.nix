30fb75856ed60212db1289b553127fe78495a24f
kzzzld <kzzzl@kzzzl.xyz>
2026-09-14 19:46:20 +0300

    pkg_add: helium-browser

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    helium = {
+      url = "github:AlvaroParker/helium-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     apple-silicon-support.url = "github:nix-community/nixos-apple-silicon";
   };
 
--- home/home.nix
+++ home/home.nix
@@ -74,6 +74,7 @@ in {
 
     # internet
     librewolf
+    inputs.helium.packages.${pkgs.system}.default
     dino
     vesktop
     signal-desktop
