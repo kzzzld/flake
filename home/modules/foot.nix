4576ba630879bc1c573cd0ea09011e4002b535ec
kzzzld <kzzzl@kzzzl.xyz>
2026-09-19 13:43:48 +0300

    feat: update foot config

--- home/modules/foot.nix
+++ home/modules/foot.nix
@@ -5,6 +5,15 @@
       main = {
         font = "JetBrainsMono Nerd Font:size=12";
         term = "xterm-256color";
+        pad = "5x5x5x5";
+      };
+
+      colors-dark = {
+          alpha = 0.9;
+      };
+
+      csd = {
+          preferred = "none";
       };
     };
   };
