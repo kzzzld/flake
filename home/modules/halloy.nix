c0f0160c3900fb0fe0fdaac194373887e025d20e
kzzzld <kzzzl@kzzzl.xyz>
2026-09-20 09:55:57 +0300

    feat: update halloy config

--- home/modules/halloy.nix
+++ home/modules/halloy.nix
@@ -3,10 +3,18 @@
     enable = true;
     settings = {
       theme = "catppuccin-mocha";
-      servers.libera = {
+      servers.Libera = {
         nickname = "kzzzl/libera";
         server = "soju.kzzzl.xyz";
-        password_command = "rbw get znc_libera";
+        password_command = "rbw get soju";
+        port = 6003;
+        use_tls = true;
+      };
+
+      servers.OFTC = {
+        nickname = "kzzzl/OFTC";
+        server = "soju.kzzzl.xyz";
+        password_command = "rbw get soju";
         port = 6003;
         use_tls = true;
       };
